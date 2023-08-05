6b03e5565a38b274b0334dc94d2dca4606fa36b0
Lotachi <lotaoko2@gmail.com>
2023-08-05 22:14:29 +0100

    Update

--- src/components/Whychoose.js
+++ src/components/Whychoose.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Circle1 from '../assets/circle1.svg' 
 import Norug from '../assets/norug.svg' 
+import Typography from '@mui/material/Typography';
+import Choosefeature from './Whychoose/Choosefeature';
 
 function Whychoose() {
   return (
 <>
-<div maxWidthd='xl' className='flex flex-col items-center  text-[#E8E8E8]  '>
+<div maxWidthd='xl' className='flex flex-col items-center  text-[#979797]  '>
 <img src={Circle1} alt="" className='w-14 lg:auto animate-pulse mb-5' />
 <img src={Norug} alt="" className='w-auto animate-pulse duration-200' />
 
-<h1 className='text-6xl  xtext-9xl sm:text-8xl lg:text-9xl font-extrabold text-center mt-32 mb-10'>
-WHY CHOOSE  FOMOBOT?
+            <h1 className='text-4xl  lg:text-5xl font-extrabold text-center mt-4 mb-4 text-[#E8E8E8] '>
+            Why Choose FOMOBOT?
             </h1>
 
+            <Typography variant='p' className='pb-14 subtitle lg:w-[590px] text-lg' sx={{ textAlign: 'center'}}>
+            Do you want to stay ahead of the crypto game and avoid potential rugpulls and scams? FOMOBOT is here to revolutionize your crypto investment journey!
+
+            </Typography>
+<Choosefeature/>
 </div>
 </>
     )
---
+++ src/components/Whychoose/Choosefeature.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Proof1 from "../../assets/proof1.png"
+
+function Choosefeature() {
+  return (
+    <>
+    <div className="divide flex flex-row">
+
+        <div className="image">
+            <img src={Proof1} alt="" />
+            
+        </div>
+        <div className="features"></div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Choosefeature
