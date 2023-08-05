bc791014d04af35055d9dbfa0b637b815408b6fa
Lotachi <lotaoko2@gmail.com>
2023-08-05 20:43:17 +0100

    Update

--- src/components/Hero/Buttons.js
+++ src/components/Hero/Buttons.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import TelegramIcon from '@mui/icons-material/Telegram';
+import ButtonBlur from '../../assets/ButtonBlur.svg'
 
 function Buttons() {
   return (
-    <div className='flex flex-col lg:flex-row'>
+    <div className='flex flex-col lg:flex-row relative'>
         
 <button type="button" className='m-2 p-2 lg:px-10 lg:py-3 lg:font-medium text-xld border rounded-xl bg-[#E8E8E8] flex justify-center text-black'>
     <TelegramIcon/>
 Telegram
 </button>
-<button type="button" className='m-2 p-2 lg:px-10 lg:py-3 lg:font-medium text-xld border border-white rounded-xl bg-black'>
+
+<div className="button  flex w-screden justify-center align-middle items-center">
+<img src={ButtonBlur} alt="" className='animate-pulse absolute -z-10 w-[500px] h-[300px] lg:w-[262px] lg:h-[126px]'/>
+<button type="button" className='w-full m-2 p-2 lg:px-10 lg:py-3 lg:font-medium text-xld border border-white rounded-xl bg-black'>
 Buy Now
 </button>
+</div>
         
     </div>
   )
