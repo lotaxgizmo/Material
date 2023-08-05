507d11c105162ff8ba05059e680b13572832416f
Lotachi <lotaoko2@gmail.com>
2023-08-05 22:30:57 +0100

    Update

--- src/components/Whychoose/Choosefeature.js
+++ src/components/Whychoose/Choosefeature.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Proof1 from "../../assets/proof1.png"
+import Proof2 from "../../assets/proof2.png"
+import Proof3 from "../../assets/proof3.png"
+import Proof4 from "../../assets/proof4.png"
+import Proof5 from "../../assets/proof5.png"
+import Proof6 from "../../assets/proof6.png"
 
 function Choosefeature() {
   return (
     <>
     <div className="divide flex flex-row">
 
-        <div className="image">
-            <img src={Proof1} alt="" />
+        <div className="image mb-10">
+            <img src={Proof1} alt="" className='p-4'/>
+            <img src={Proof2} alt="" className='p-4'/>
+            
+            <img src={Proof3} alt="" className='p-4 lg:hidden'/>
+            <img src={Proof6} alt="" className='p-4 lg:hidden'/>
+            {/* <img src={Proof5} alt="" className='p-4 lg:hidden'/>
+            <img src={Proof4} alt="" className='p-4 lg:hidden'/> */}
             
         </div>
         <div className="features"></div>
